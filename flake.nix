f3e4b6f3c240fdd0a9ce989f8e902e0104de2b4d
hugosenari <hugosenari@gmail.com>
2022-03-12 03:50:49 -0300

    chore: re index package_visible_types

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A hacky helper lib for authoring Nim packages with package-level visiblity'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."package_visible_types-master".url = "path:./master";
+  inputs."package_visible_types-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."package_visible_types-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A hacky helper lib for authoring Nim packages with package-level visiblity'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-package_visible_types-master.flake = false;
+  inputs.src-package_visible_types-master.owner = "zah";
+  inputs.src-package_visible_types-master.ref   = "refs/heads/master";
+  inputs.src-package_visible_types-master.repo  = "nim-package-visible-types";
+  inputs.src-package_visible_types-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-package_visible_types-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-package_visible_types-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
